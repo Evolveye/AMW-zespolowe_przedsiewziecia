1fd4856911655feaaecbe90d680b3211a47ebbfd
Adam Sztandar <sztandaradam@gmail.com>
2020-11-30 20:39:55 +0100

    Make base layout #SASS-20

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,21 +2,28 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import LoginForm from "../components/login"
+import RegisterForm from "../components/register"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="wrapper">
+        <div className="wstep-div" id="wstep">
+           <div className="wstep-div-text">
+             <p className="wstep-div-text-p">Tu będzie jakiś ładny slogan</p>
+           </div>
+        </div>
+        <div className="logowanie-div" id="logowanie">
+           <LoginForm />
+
+        </div>
+        <div className="rejestracja-div"  id="rejestracja">
+         <RegisterForm />
+        </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
-)
+) 
 
 export default IndexPage
