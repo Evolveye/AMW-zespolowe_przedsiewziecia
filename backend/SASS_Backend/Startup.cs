403eccee877a7635c3bc6b9c789ea56a261bb85b
AdamSzr <adam.szr98@gmail.com>
2020-12-01 16:37:09 +0100

    Server http #SASS-24

--- backend/SASS_Backend/Startup.cs
+++ backend/SASS_Backend/Startup.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace SASS_Backend
@@ -25,15 +27,36 @@ namespace SASS_Backend
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseHttpsRedirection();
+
+            var options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("index.html");
+            app.UseDefaultFiles(options);
+
+            app.UseStaticFiles();//The default directory is {content root}/wwwroot
+
+            app.UseStaticFiles(new StaticFileOptions // public directory, made to upload and dowlnoad files
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(env.ContentRootPath, "public")),
+                RequestPath = "/public"
+            });
+
+            app.UseDirectoryBrowser(new DirectoryBrowserOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(env.ContentRootPath, "public")),
+                RequestPath = "/public"
+            });
+
 
             app.UseRouting();
 
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+               // endpoints.MapControllers();
             });
         }
     }
