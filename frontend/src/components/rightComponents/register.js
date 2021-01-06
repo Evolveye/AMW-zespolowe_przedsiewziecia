f956ca2677b1e1ee92b4143a3478df7ae4167400
Adam Sztandar <sztandaradam@gmail.com>
2021-01-06 15:10:18 +0100

    poprawienie rozjeżdzającego się formularza #SASS-18

--- frontend/src/components/rightComponents/register.js
+++ frontend/src/components/rightComponents/register.js
@@ -5,71 +5,92 @@ import { navigate } from "gatsby"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUser, faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons"
 
+import { DEBUG, BACKEND_REGISTER_URL } from "../../config.json"
 
-class RightContainerRegister extends React.Component { 
 
+class RightContainerRegister extends React.Component {
 
+  /**
+   *
+   * @param {Subm} event
+   */
   handleSubmit = event => {
     event.preventDefault();
-    navigate('/checkMail');
+
+    if (DEBUG) navigate('/checkMail')
+    else fetch( BACKEND_REGISTER_URL, { method:`POST`, body: {
+      name: ``,
+      surname: ``,
+      email: ``,
+      password: ``,
+      password2: ``,
+    } } )
+      // .then( res => res.json() )
+      // .then( data => {
+      //   // ...
+
+      //   navigate('/checkMail')
+      // } )
+      .then( res => res.text() )
+      .then( console.log )
+      .catch( console.error )
   }
 
 
-  render() {
-    return (
-      <div className="right-container">
-        <div className="login-form">
-          <div className="login-form-header">
-            <Link to="/">
-              {" "}
-              <div className="logo-mini"></div>{" "}
-            </Link>
-            <div className="login-form-header-div-text">
-              <span className="login-form-header-text">
-                Platforma edukacyjna - rejestracja
-              </span>
-            </div>
+  render = () => (
+    <div className="right-container">
+      <div className="login-form">
+        <div className="login-form-header">
+          <Link to="/">
+            {" "}
+            <div className="logo-mini"></div>{" "}
+          </Link>
+          <div className="login-form-header-div-text">
+            <span className="login-form-header-text">
+              Platforma edukacyjna - rejestracja
+            </span>
           </div>
-          <div className="login-box">
-            <div className="login-box-form">
-              <form method="post" onSubmit={this.handleSubmit}>
-                <div className="textbox">
-                  <FontAwesomeIcon icon={faUser} size="2px" />
-                  <input type="text" name="username" placeholder="Imię" />
-                </div>
+        </div>
+        <div className="login-box">
+          <div className="login-box-form">
+            <form method="post" onSubmit={this.handleSubmit}>
+              <div className="textbox">
+                <FontAwesomeIcon icon={faUser} size="1x" />
+                <input type="text" name="name" placeholder="Imię" />
+              </div>
 
-                <div className="textbox">
-                  <input type="text" name="surname" placeholder="Nazwisko" />
-                </div>
+              <div className="textbox">
+                <input className="padding-left" type="text" name="surname" placeholder="Nazwisko" />
+              </div>
 
-                <div className="textbox under-space">
-                  <FontAwesomeIcon icon={faEnvelope} size="2px" />
-                  <input type="text" name="email" placeholder="E-mail" />
-                </div>
+              <div className="textbox under-space">
+                <FontAwesomeIcon icon={faEnvelope} size="1x" />
+                <input type="text" name="email" placeholder="E-mail" />
+              </div>
 
-                <div className="textbox under-space">
-                  <FontAwesomeIcon icon={faLock} size="2px" />
-                  <input type="password" name="password" placeholder="Hasło" />
-                </div>
+              <div className="textbox under-space">
+                <FontAwesomeIcon icon={faLock} size="1x" />
+                <input type="password" name="password" placeholder="Hasło" />
+              </div>
 
-                <div className="textbox">
-                  <input
-                    type="password"
-                    name="password2"
-                    placeholder="Powtórz hasło"
-                  />
-                </div>
+              <div className="textbox">
+                <input
+                  className="padding-left"
+                  type="password"
+                  name="password2"
+                  placeholder="Powtórz hasło"
+                />
+              </div>
 
-                <div className="register-box-submit">
-                  <input type="submit" value="Zarejestruj się" />
-                </div>
-              </form>
-            </div>
+              <div className="register-box-submit">
+                <input type="submit" value="Zarejestruj się" />
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 export default RightContainerRegister
 /*
@@ -92,7 +113,7 @@ const RightContainerRegister = () => (
               <input type="text" name="username" placeholder="Imię" />
             </div>
 
-            <div className="textbox"> 
+            <div className="textbox">
               <input type="text" name="surname" placeholder="Nazwisko" />
             </div>
 
@@ -106,12 +127,12 @@ const RightContainerRegister = () => (
               <input type="password" name="password" placeholder="Hasło" />
             </div>
 
-            <div className="textbox"> 
+            <div className="textbox">
               <input type="password" name="password2" placeholder="Powtórz hasło" />
             </div>
 
-            <div className="register-box-submit"> 
-              <input type="button" value="Zarejestruj się" /> 
+            <div className="register-box-submit">
+              <input type="button" value="Zarejestruj się" />
             </div>
           </form>
         </div>
