47def6de5570428fdba10dec0b53972537d4648f
Adam Sztandar <sztandaradam@gmail.com>
2021-01-06 23:45:17 +0100

    działająca rejestracja #SASS-18

--- frontend/src/components/rightComponents/register.js
+++ frontend/src/components/rightComponents/register.js
@@ -6,34 +6,46 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUser, faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons"
 
 import { DEBUG, BACKEND_REGISTER_URL } from "../../config.json"
+import { handleForm } from "../formsHandling.js"
 
 
 class RightContainerRegister extends React.Component {
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: ``,
+      surname: ``,
+      email: ``,
+      password1: ``,
+      password2: ``
+    };
+  }
   /**
    *
    * @param {Subm} event
    */
+
+  handleInputChange = (event) => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
   handleSubmit = event => {
     event.preventDefault();
 
     if (DEBUG) navigate('/checkMail')
-    else fetch( BACKEND_REGISTER_URL, { method:`POST`, body: {
-      name: ``,
-      surname: ``,
-      email: ``,
-      password: ``,
-      password2: ``,
-    } } )
-      // .then( res => res.json() )
-      // .then( data => {
-      //   // ...
-
-      //   navigate('/checkMail')
-      // } )
-      .then( res => res.text() )
-      .then( console.log )
-      .catch( console.error )
+    else handleForm(
+      BACKEND_REGISTER_URL,
+      'post',
+      { "Content-Type": 'application/json' },
+      this.state,
+      {
+        okCb(){
+          navigate('/checkMail')
+        }
+      }
+    )
   }
 
 
@@ -56,21 +68,46 @@ class RightContainerRegister extends React.Component {
             <form method="post" onSubmit={this.handleSubmit}>
               <div className="textbox">
                 <FontAwesomeIcon icon={faUser} size="1x" />
-                <input type="text" name="name" placeholder="Imię" />
+                <input
+                type="text"
+                name="name"
+                placeholder="Imię"
+                value={this.state.name}
+                onChange={this.handleInputChange}
+                required />
               </div>
 
               <div className="textbox">
-                <input className="padding-left" type="text" name="surname" placeholder="Nazwisko" />
+                <input
+                className="padding-left"
+                type="text"
+                name="surname"
+                placeholder="Nazwisko"
+                value={this.state.surname}
+                onChange={this.handleInputChange}
+                required />
               </div>
 
               <div className="textbox under-space">
                 <FontAwesomeIcon icon={faEnvelope} size="1x" />
-                <input type="text" name="email" placeholder="E-mail" />
+                <input
+                type="text"
+                name="email"
+                placeholder="E-mail"
+                value={this.state.email}
+                onChange={this.handleInputChange}
+                required />
               </div>
 
               <div className="textbox under-space">
                 <FontAwesomeIcon icon={faLock} size="1x" />
-                <input type="password" name="password" placeholder="Hasło" />
+                <input
+                type="password"
+                name="password1"
+                placeholder="Hasło"
+                value={this.state.password1}
+                onChange={this.handleInputChange}
+                required />
               </div>
 
               <div className="textbox">
@@ -79,7 +116,9 @@ class RightContainerRegister extends React.Component {
                   type="password"
                   name="password2"
                   placeholder="Powtórz hasło"
-                />
+                  value={this.state.password2}
+                  onChange={this.handleInputChange}
+                  required />
               </div>
 
               <div className="register-box-submit">
