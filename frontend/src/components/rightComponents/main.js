4b942d1b7941f1cdaa5b727f8f1b62de8b06b2c1
Adam Sztandar <sztandaradam@gmail.com>
2021-01-12 14:23:15 +0100

    zamiana na klasę #SASS-20

--- frontend/src/components/rightComponents/main.js
+++ frontend/src/components/rightComponents/main.js
@@ -1,6 +1,36 @@
-import React from "react"; 
+import React from "react";
+import { navigate } from "gatsby"
+import { isLoggedIn } from "../../services/auth"
 
-const RightContainerMain = () => (
+class RightContainerMain extends React.Component {
+
+    state = {
+        rerender: true
+    }
+    componentDidMount() {
+        this.setState( { rerender:false } )
+    }
+    render() {
+        if (this.state.rerender && isLoggedIn()) navigate(`/users/me`)
+        return <>
+            <div className="right-container">
+                <div className="index-logo">
+                    <div className="logo"></div>
+                    <span className="logo-text1">Platforma Edukacyjna</span>
+                    <span className="logo-text2">Edukacja, szkolenia, spotkania, kursy</span>
+                </div>
+            </div>
+        </>
+    }
+}
+export default RightContainerMain
+
+
+
+
+  /*
+  const RightContainerMain = () => (
+
     <div className="right-container">
             <div className="index-logo">
                 <div className="logo"></div>
@@ -9,5 +39,6 @@ const RightContainerMain = () => (
             </div>
     </div>
   )
-  
+
   export default RightContainerMain
+  */
