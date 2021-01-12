2d5e26a90aa0a2a060f7866d8a9b56cef8ab1d98
Adam Sztandar <sztandaradam@gmail.com>
2021-01-12 14:45:35 +0100

    pobieranie danych z api #SASS-110

--- frontend/src/components/rightComponents/settings.js
+++ frontend/src/components/rightComponents/settings.js
@@ -1,18 +1,20 @@
-import React from "react" 
-import { getUser } from "../../services/auth"
-
-
+import React from "react"
+import socket from "../../services/webSocket.js"
 
 export default class Settings extends React.Component {
-  user = getUser()
+
+  user = Settings.getData()
 
   handleRef = ref => {
-    this.user.then( ({ email, name, surname, login, avatarSrc }) => {
-      ref.querySelector( `[name="login"]` ).placeholder = login
-      ref.querySelector( `[name="email"]` ).placeholder = email
-      ref.querySelector( `[name="name"]` ).placeholder = name
-      ref.querySelector( `[name="surname"]` ).placeholder = surname
-      // ref.querySelector( `.avatar-image` ).src = avatarSrc
+    this.user.then( ({ name, surname, email, login, avatarSrc }) => {
+      console.info("dane jakie są w this.user.then(({email, name, surname, login, avatarSrc })) ", { name, surname, email, login, avatarSrc } )
+      console.log( ref.querySelector( `[name="login"]` ) )
+      ref.querySelector( `[name="login"]` ).value = login
+      ref.querySelector( `[name="email"]` ).value = email
+      ref.querySelector( `[name="name"]` ).value = name
+      ref.querySelector( `[name="surname"]` ).value = surname
+      ref.querySelector( `.avatar-image` ).src = avatarSrc
+      ref.querySelector( `[name="loginMerge"]` ).value = ""
     } )
   }
 
@@ -22,7 +24,7 @@ export default class Settings extends React.Component {
         <img
           className="avatar-image"
           src="https://upload.wikimedia.org/wikipedia/commons/5/55/Marcin_Najman_2014.jpg"
-          alt=""
+          alt={this.avatarSrc}
         />
         <span className="avatar-container-text">
           Kliknij aby zmienić profilowe
@@ -93,6 +95,7 @@ export default class Settings extends React.Component {
               type="button"
               name="loginMergeRequest"
               value="Wyślij żądanie scalenia"
+              autoComplete="off"
             />
           </div>
         </div>
@@ -102,13 +105,13 @@ export default class Settings extends React.Component {
         <div className="settings-form-element">
           <div className="settings-form-element-label">Nowe hasło</div>
           <div className="settings-form-element-input">
-            <input type="password" name="password" />
+            <input type="password" name="password1" autoComplete="off" />
           </div>
         </div>
         <div className="settings-form-element">
           <div className="settings-form-element-label">Powtórz nowe hasło</div>
           <div className="settings-form-element-input">
-            <input type="password" name="repassword" />
+            <input type="password" name="password2" autoComplete="off" />
           </div>
         </div>
         <div className="settings-form-element">
@@ -116,7 +119,7 @@ export default class Settings extends React.Component {
             Potwierdź starym hasłem
           </div>
           <div className="settings-form-element-input">
-            <input type="password" name="oldpassword" />
+            <input type="password" name="password0" autoComplete="off" />
           </div>
         </div>
         <div className="settings-form-element">
@@ -128,4 +131,12 @@ export default class Settings extends React.Component {
       </form>
     </div>
   </div>
+
+static getData() {
+  if(!socket) return new Promise(res => res([]))
+  else return new Promise(resolve => {
+    socket.on(`api.get.me`, resolve)
+    socket.emit(`api.get.me`)
+  }).catch( (error) => console.error( `api.get.users.me ::`, error ) );
+}
 }
