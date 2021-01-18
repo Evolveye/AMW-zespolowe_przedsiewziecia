544f32c9020e9dbce14d3706b4112543be263a42
Adam Sztandar <sztandaradam@gmail.com>
2021-01-18 13:42:41 +0100

    odczytywanie ocen z bazy danych #SASS-132

--- frontend/src/components/rightComponents/myGrades.js
+++ frontend/src/components/rightComponents/myGrades.js
@@ -1,13 +1,17 @@
 import React from "react"
 import socket from "../../services/webSocket.js"
+import { getToken } from "../../services/auth"
+import { BACKEND_PLATFORMS_USERS_NOTES } from "../../config"
 
 class RightContainerMyGrades extends React.Component {
   state = {
     grades: [],
+    platformList: [],
   }
 
   data = {
     grades: [],
+    average:'',
   }
 
   avgE = 0
@@ -16,44 +20,99 @@ class RightContainerMyGrades extends React.Component {
 
   average = array => array.reduce((a, b) => a + b) / array.length
 
+  handlePlatformClick = (platformTag, groups) => {
+    alert("kliknąłeś")
+    console.log("groups: ", groups)
+    const table = groups.map(({ group, notes }, index) => {
+      console.log("values: ", {notes})
+      const average =
+       notes.reduce((avg, { value }) => parseFloat(avg) + parseFloat(value), 0) / notes.length 
+      // const average = notes.reduce(function(avg, {value}){
+      //  console.log("average function - avg: ", avg)
+      //  console.log("average function - value: ", value)
+      //  console.log("avg + value", avg + value)
+      //  return avg + value
+      // },0)
+      console.log("notes length: ", notes.length)
+      console.log("average: ", average)
+      const notesElements = notes.map(({ value }, index) => <span key={index}>{value},</span>) 
+      this.data.grades.push(average)
+      console.log("this data grades average: ", this.data.grades)
+      this.data.average = this.data.grades.reduce((a, b) => a + b, 0) / this.data.grades.length
+      console.log("this data average: ", this.data.average)
+      console.log("notesElements: ", notesElements)
+
+      return (
+        <div className="grades-gained-container-grid-new-row" key={index}>
+          <span className="grid-item przedmiot">{group.name}</span>
+          <span className="grid-item prowadzacy">
+            {group.lecturer.name} {group.lecturer.surname}
+          </span>
+          <span className="grid-item oceny">{notesElements}</span>
+          <span className="grid-item ocena-koncowa">{average.toFixed(2)}</span>
+        </div>
+      )
+    })
+    this.setState({grades: table});
+    console.log("this state: ", this.state.grades)
+    //ref.innerHTML = table
+  }
+
   componentDidMount() {
     RightContainerMyGrades.getData()
-      .then(arr =>
-        arr.map((a, index) => (
-          <div className="grades-gained-container-grid-new-row" key={index}>
-            <span className="grid-item przedmiot">{a.przedmiot}</span>
-            <span className="grid-item prowadzacy">{a.prowadzacy}</span>
-            <span className="grid-item oceny">
-              {JSON.stringify(a.oceny).slice(1, -1)}
-            </span>
-            <span className="grid-item ocena-koncowa">
-              {this.average(a.oceny).toFixed(2)}
-            </span>
+      .then(({ data }) => {
+        console.log("dane:", data)
+        return data.map((platform, index) => (
+          <div className="subject" key={index}>
+            <div className="subject-icon"></div>
+            <div
+              className="subject-name"
+              onClick={tag => this.handlePlatformClick(tag, platform.groups)}
+            >
+              {platform.platform.name}
+            </div>
           </div>
         ))
-      )
-      .then(grades => this.setState({ grades }))
-      .then(() => {
-        /** @type {HTMLElement} */
-        const gradesElement = this.gradesElement.current
-        const gradesSelector = `.grades-gained-container .grades-gained-container-grid-new-row .grid-item.oceny`
-        const allGrades = Array.from(
-          gradesElement.querySelectorAll(gradesSelector)
-        )
+      })
+      .then(platformList => this.setState({ platformList }))
 
-        this.data.grades = allGrades.map(({ textContent }) =>
-          textContent.split(/[, ]+/).map(Number)
-        )
-      })
-      .then(() => {
-        for (let i = 0; i < this.data.grades.length; i++) {
-          this.avgE += this.average(this.data.grades[i])
-        }
-        this.avgE = this.avgE / this.data.grades.length
-        document.getElementsByClassName(
-          "srednia-koncowa"
-        )[0].innerHTML += this.avgE.toFixed(2)
-      })
+    // RightContainerMyGrades.getData()
+    //   .then(({notes}) =>
+    //     notes.map((a, index) => (
+    //       <div className="grades-gained-container-grid-new-row" key={index}>
+    //         <span className="grid-item przedmiot">{a.przedmiot}</span>
+    //         <span className="grid-item prowadzacy">{a.prowadzacy}</span>
+    //         <span className="grid-item oceny">
+    //           {a.oceny.slice(1, -1)}
+    //         </span>
+    //         <span className="grid-item ocena-koncowa">
+    //           {this.average(a.oceny).toFixed(2)}
+    //         </span>
+    //       </div>
+    //     ))
+    //   )
+    //   .then(grades => this.setState({ grades }))
+    //   .then(() => {
+    //     /** @type {HTMLElement} */
+    //     const gradesElement = this.gradesElement.current
+    //     const gradesSelector = `.grades-gained-container .grades-gained-container-grid-new-row .grid-item.oceny`
+    //     const allGrades = Array.from(
+    //       gradesElement.querySelectorAll(gradesSelector)
+    //     )
+
+    //     this.data.grades = allGrades.map(({ textContent }) =>
+    //       textContent.split(/[, ]+/).map(Number)
+    //     )
+    //   })
+    //   .then(() => {
+    //     for (let i = 0; i < this.data.grades.length; i++) {
+    //       this.avgE += this.average(this.data.grades[i])
+    //     }
+    //     this.avgE = this.avgE / this.data.grades.length
+    //     document.getElementsByClassName(
+    //       "srednia-koncowa"
+    //     )[0].innerHTML += this.avgE.toFixed(2)
+    //   })
   }
 
   render = () => (
@@ -61,17 +120,13 @@ class RightContainerMyGrades extends React.Component {
       <div className="right-container-profile">
         <div className="right-container-profile-left">
           <div className="right-container-profile-left-text">
-            <span>Przypięte przedmioty</span>
+            <span>Platformy</span>
+            <br/>
+            <span>z ocenami</span>
           </div>
           <div className="subjects-list">
-            <div className="subject">
-              <div className="subject-icon"></div>
-              <div className="subject-name">Platforma A</div>
-            </div>
-            <div className="subject">
-              <div className="subject-icon"></div>
-              <div className="subject-name">Platofrma BC</div>
-            </div>
+            
+            {this.state.platformList}
           </div>
         </div>
 
@@ -117,7 +172,7 @@ class RightContainerMyGrades extends React.Component {
                   <span className="empty empty1"></span>
                   <span className="empty empty2"></span>
                   <span className="empty empty3"></span>
-                  <span className="grid-item srednia-koncowa"></span>
+                  <span className="grid-item srednia-koncowa">{Number(this.data.average).toFixed(2)}</span>
                 </div>
               </div>
             </div>
@@ -128,6 +183,15 @@ class RightContainerMyGrades extends React.Component {
   )
 
   static getData() {
+    return fetch(BACKEND_PLATFORMS_USERS_NOTES, {
+      method: "GET",
+      headers: {
+        Authentication: `Bearer ${getToken()}`,
+      },
+    }).then(res => res.json())
+  }
+  /*
+  static getData() {
     if (!socket) return new Promise(res => res([]))
     else
       return new Promise(resolve => {
@@ -136,5 +200,7 @@ class RightContainerMyGrades extends React.Component {
         // console.error("jesteś w moich ocenach, po 'socket.on(`api.get.users.me.notes`, resolve) socket.emit(`api.get.users.me.notes`)'")
       }).catch(console.error)
   }
+*/
 }
+
 export default RightContainerMyGrades
