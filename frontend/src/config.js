a82c635b32b645cd486cd0f37f3e20119ad02f6c
Adam Sztandar <sztandaradam@gmail.com>
2021-01-19 23:37:14 +0100

    dodanie dodatkowych adresów endpoint #SASS-44

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -25,10 +25,18 @@ export const BACKEND_PLATFORMS_GROUPS_ADD = `${HOST}/api/groups`
 export const BACKEND_PLATFORMS_GROUPS_DEL = `${HOST}/api/groups/:groupId`
 export const BACKEND_PLATFORMS_USERS_NOTES = `${HOST}/api/groups/notes`
 export const BACKEND_PLATFORMS_USERS_ADD = `${HOST}/api/groups/users`
+export const BACKEND_PLATFORMS_GROUP_USERS_GET = `${HOST}/api/groups/:groupId/users`
 export const BACKEND_PLATFORMS_GROUPS_USER_NOTES_GET = `${HOST}/api/groups/:groupId/notes`
 export const BACKEND_PLATFORMS_GROUPS_USER_NOTES_DEL = `${HOST}/api/groups/notes/:noteId`
-export const BACKEND_PLATFORMS_GROUPS_USER_NOTES_ADD = `${HOST}/api/groups/notes`
+export const BACKEND_PLATFORMS_GROUPS_USER_NOTES_ADD = `${HOST}/api/groups/:groupId/notes/`
 export const BACKEND_PLATFORMS_GROUPS_USER_NOTES_PUT = `${HOST}/api/groups/notes/:noteId`
+
+export const BACKEND_PLATFORMS_GROUPS_MEET_ADD = `${HOST}/api/meets`
+export const BACKEND_PLATFORMS_GROUPS_MEET_GET = `${HOST}/api/meets`
+export const BACKEND_PLATFORMS_GROUPS_MEET_DEL = `${HOST}/api/meets/:meetId`
+export const BACKEND_PLATFORMS_GROUPS_MEET = `${HOST}/api/meets/group/:groupId`
+export const BACKEND_PLATFORMS_GROUPS_MEET_ADD_USER = `${HOST}/api/meets/:meetId/users`
+export const BACKEND_PLATFORMS_GROUPS_MEET_GET_USER = `${HOST}/api/meets/:meetId/users`
 
 export const BACKEND_CALENDAR_URL = "/api/calendar"
 
