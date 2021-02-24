662e89328b56802dcaf8e687884342c6e980df69
Evolveye <stolarski.paw@gmail.com>
2021-02-24 16:35:38 +0100

    Clean the project root #SASS-188

--- backend/index.js
+++ backend/index.js
@@ -1,189 +1,210 @@
-import express from "express";
-import { graphqlHTTP } from "express-graphql";
-import mongose from "mongoose";
-import { Server } from "socket.io";
-import cors from "cors";
-import { GraphQLSchema, GraphQLObjectType } from "graphql";
+import express from "express"
+import { graphqlHTTP } from "express-graphql"
+import mongose from "mongoose"
+import cors from "cors"
+import { GraphQLSchema, GraphQLObjectType } from "graphql"
 
-import WSS from "./priv/src/ws.js";
-import dbManager from "./priv/src/dbManager.js";
-import { DEBUG, APP_ROOT_DIR, PORT, LOGGERS } from "./priv/consts.js"; // LOGGERS
+import WSS from "./priv/src/ws.js"
+import dbManager from "./priv/src/dbManager.js"
+import { DEBUG, PORT, LOGGERS } from "./priv/consts.js"
 import {
   doHttpLogShouldBePrinted as doHttpLog,
-  doWsLogShouldBePrinted as doWsLog,
+  // doWsLogShouldBePrinted as doWsLog,
   logUnderControl as log,
-  addNextLineToLog as logLine,
-} from "./priv/src/utils.js";
+  // addNextLineToLog as logLine,
+} from "./priv/src/utils.js"
 
 /** @typedef {import("./modules/module.js").default[]} Module */
 
-const { connect } = mongose;
-const DB_NAME = `SassPE`;
-const DB_CONN_STRING = `mongodb://127.0.0.1:27017/${DB_NAME}`;
 
-const mongoose = await connect(DB_CONN_STRING, {
+
+/*\
+ *   Variables
+\*/
+
+
+
+const DB_NAME = `SassPE`
+const DB_CONN_STRING = `mongodb://127.0.0.1:27017/${DB_NAME}`
+
+await mongose.connect( DB_CONN_STRING, {
   autoIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-});
+} )
 
-/* *
- *  Variables
- */
+const app = express()
+const modulesClasses = await makeModulesClasses( `user`, `platform` )
+const modulesInstances = makeModulesInstances( modulesClasses )
+const server = app.listen( PORT, () => log( LOGGERS.server, `Working localhost:${PORT}` ) )
+const wss = new WSS({ server })
 
-/** @type {Express} */
-const app = express();
 
-const moduleLogger = (modName) => (string) =>
-  log(LOGGERS.module, modName, string);
-const importModule = (modName) => import(`./addons/${modName}/index.js`);
 
-/** @type {Map<string,Module>} */
-const modulesClasses = new Map(
-  await Promise.all([
-    importModule(`user`),
-    importModule(`platform`),
-    // importModule(`group`),
-    // importModule(`meet`),
-  ]).then((mods) => mods.map(({ default: d }) => [d.toString(), d]))
-);
-/** @type {Map<string,Module>} */
-const modulesInstances = new Map();
+/*\
+ *   Configuration
+\*/
 
-const server = app.listen(PORT, () =>
-  log(LOGGERS.server, `Working localhost:${PORT}`)
-);
-// const wss = new WSS({ server });
 
-/* *
- *  Configuration
- */
 
-// filter modules by "requiredModules"
-modulesClasses.forEach((Class, modName) => {
-  const requiredModules = {};
-  let doInstallation = true;
+app.use( (req, _, next) => next(
+  doHttpLog( req ) && log( LOGGERS.newRequest, `HTTP`, req.method, req.url ) ),
+)
 
-  for (const requiredModule of Class.requiredModules) {
-    if (modulesInstances.has(requiredModule)) {
-      requiredModules[requiredModule] = modulesInstances.get(requiredModule);
-    } else {
-      doInstallation = false;
-      break;
-    }
-  }
+app.use( `/`, express.static( DEBUG ? `./public` : `../frontend` ) )
+app.use( `/media`, express.static( `./media` ) )
 
-  if (doInstallation) {
-    modulesInstances.set(
-      modName,
-      new Class(moduleLogger(modName), dbManager, requiredModules)
-    );
-  }
-});
+app.use( cors() )
+app.use( express.json() )
 
-// add to modules its "additionalModules"
-modulesInstances.forEach((instance, modName) =>
-  /** @type {string[]} */ (modulesClasses.get(modName).additionalModules)
-    .filter((optModName) => modulesInstances.has(optModName))
-    .map((optModName) => modulesInstances.get(optModName))
-    .forEach(instance.addAdditionalModule)
-);
+app.use( `/api/graphql`, getModulesGraphQlConfig( modulesInstances ) )
 
-app.use((req, _, next) =>
-  next(
-    // logging middleware
-    doHttpLog(req)
-      ? log(LOGGERS.newRequest, `HTTP`, req.method, req.url)
-      : undefined
+app.use( (_, res) => res.status( 404 ).json({ code:0, error:`Endpoint not found` }) )
+
+
+
+/*\
+ *   Functions
+\*/
+
+
+
+/** @return {Promise<Map<string,Module>>} */
+async function makeModulesClasses( ...addonsNames ) {
+  return new Map(
+    await Promise.all( addonsNames.map( name => import(`./addons/${name}/index.js`) ) )
+      .then( mods => mods.map( ({ default:d }) => [ d.toString(), d ] ) ),
   )
-);
+}
 
-app.use(`/`, express.static(DEBUG ? `./public` : `../frontend`));
-app.use(`/media`, express.static(`./media`));
 
-app.use(cors());
-app.use(express.json());
 
-// install modules configuration
-const graphQlConfig = { queryObj: {}, mutationObj: {} };
-modulesInstances.forEach((mod) => {
-  log(LOGGERS.server, `[fgYellow]LOADING MODULE[] ${mod.toString()}`);
-  const { graphQl } = mod.getApi();
+/** @return {Map<string,Module>} */
+function makeModulesInstances( modulesClasses ) {
+  const moduleLogger = modName => string => log( LOGGERS.module, modName, string )
+  const modulesInstances = new Map()
 
-  Object.assign(graphQlConfig.queryObj, graphQl.queryObj);
-  Object.assign(graphQlConfig.mutationObj, graphQl.mutationObj);
+  modulesClasses.forEach( (Class, modName) => {
+    const requiredModules = {}
+    let doInstallation = true
 
-  // mod.getApi().forEach((methods, path) => {
-  //   path = path.match( /\/?(?:api)?\/?(.+)/ )[ 1 ]
+    for (const requiredModule of Class.requiredModules) {
+      if (modulesInstances.has( requiredModule )) {
+        requiredModules[ requiredModule ] = modulesInstances.get( requiredModule )
+      } else {
+        doInstallation = false
+        break
+      }
+    }
 
-  //   if (typeof methods === `function`) {
-  //     logLine( `[fgYellow]/api/${path}[] :: GET`)
+    if (doInstallation && !modulesInstances.has( modName )) {
+      modulesInstances.set(
+        modName,
+        new Class(moduleLogger( modName ), dbManager, requiredModules),
+      )
+    }
+  } )
 
-  //     return app.get(`/api/${path}`, methods )
-  //   }
+  return modulesInstances
+}
 
-  //   const includedMethods = Object.keys(methods)
-  //     .filter(method => method in app)
-  //     .filter(method => typeof methods[method] === `function`)
-  //     .map(method => {
-  //       app[method](`/api/${path}`, methods[method] )
 
-  //       return method.toUpperCase()
-  //     })
 
-  //   logLine( `[fgYellow]/api/${path}[] :: ${includedMethods.join( ` ` )}`)
-  // })
-});
-app.use(
-  `/api/graphql`,
-  (() => {
-    const { queryObj, mutationObj } = graphQlConfig;
+function setModulesAdditionalModules( modulesInstances ) { // eslint-disable-line
+  modulesInstances.forEach( (instance, modName) =>
+    /** @type {string[]} */ (modulesClasses.get( modName ).additionalModules)
+      .filter( optModName => modulesInstances.has( optModName ) )
+      .map( optModName => modulesInstances.get( optModName ) )
+      .forEach( instance.addAdditionalModule ),
+  )
+}
 
-    const query = new GraphQLObjectType({
-      name: `QUERY`,
-      fields: queryObj,
-    });
 
-    const mutation = new GraphQLObjectType({
-      name: `MUTATION`,
-      fields: mutationObj,
-    });
+function getModulesGraphQlConfig( modulesInstances ) {
+  const queryObj = {}
+  const mutationObj = {}
 
-    return graphqlHTTP({
-      schema: new GraphQLSchema({ query, mutation }),
-      graphiql: true,
-    });
-  })()
-);
+  modulesInstances.forEach( mod => {
+    log( LOGGERS.server, `[fgYellow]LOADING MODULE[] ${mod.toString()}` )
 
-app.use((_, res) =>
-  res.status(404).json({ code: 0, error: `Endpoint not found` })
-);
+    const { graphQl } = mod.getApi()
 
-// wss.on(`connection`, (ws) => {
-//   const socket = wss.reshapeWebSocket(ws);
+    Object.assign( queryObj, graphQl.queryObj )
+    Object.assign( mutationObj, graphQl.mutationObj )
+  } )
 
-//   log(LOGGERS.newRequest, `WS`, `[fgGreen]New socket[]`, socket.id);
+  const query = new GraphQLObjectType({
+    name: `QUERY`,
+    fields: queryObj,
+  })
 
-//   socket.addMiddleware((event, data) => {
-//     if (!doWsLog()) return;
+  const mutation = new GraphQLObjectType({
+    name: `MUTATION`,
+    fields: mutationObj,
+  })
 
-//     const dataStr =
-//       data === event || !data
-//         ? `[[fgWhite]no data[]]`
-//         : typeof data === `string` || Array.isArray(data)
-//         ? data
-//         : Object.keys(data);
+  return graphqlHTTP({
+    schema: new GraphQLSchema({ query, mutation }),
+    graphiql: true,
+  })
+}
 
-//     log(LOGGERS.newRequest, `WS`, event, dataStr);
-//   });
-//   socket.on(
-//     `disconnect`,
-//     () =>
-//       doWsLog() &&
-//       log(LOGGERS.newRequest, `WS`, `[fgGreen]Socket left[]`, socket.id)
-//   );
 
-//   modulesInstances.forEach((mod) => mod.socketConfigurator(socket));
-// });
+
+/*\
+ *   Unused
+\*/
+
+
+
+
+// mod.getApi().forEach((methods, path) => {
+//   path = path.match( /\/?(?:api)?\/?(.+)/ )[ 1 ]
+
+//   if (typeof methods === `function`) {
+//     logLine( `[fgYellow]/api/${path}[] :: GET`)
+
+//     return app.get(`/api/${path}`, methods )
+//   }
+
+//   const includedMethods = Object.keys(methods)
+//     .filter(method => method in app)
+//     .filter(method => typeof methods[method] === `function`)
+//     .map(method => {
+//       app[method](`/api/${path}`, methods[method] )
+
+//       return method.toUpperCase()
+//     })
+
+//   logLine( `[fgYellow]/api/${path}[] :: ${includedMethods.join( ` ` )}`)
+// })
+
+
+
+wss.on( `connection`, () => {
+  // const socket = wss.reshapeWebSocket(ws);
+
+  // log(LOGGERS.newRequest, `WS`, `[fgGreen]New socket[]`, socket.id);
+
+  // socket.addMiddleware((event, data) => {
+  //   if (!doWsLog()) return;
+
+  //   const dataStr =
+  //     data === event || !data
+  //       ? `[[fgWhite]no data[]]`
+  //       : typeof data === `string` || Array.isArray(data)
+  //       ? data
+  //       : Object.keys(data);
+
+  //   log(LOGGERS.newRequest, `WS`, event, dataStr);
+  // });
+  // socket.on(
+  //   `disconnect`,
+  //   () =>
+  //     doWsLog() &&
+  //     log(LOGGERS.newRequest, `WS`, `[fgGreen]Socket left[]`, socket.id)
+  // );
+
+  // modulesInstances.forEach((mod) => mod.socketConfigurator(socket));
+} )
