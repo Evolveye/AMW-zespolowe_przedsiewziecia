64621d84da388274a056e62f41aad48a24f5069c
Adam Sztandar <10097355+asztandar@users.noreply.github.com>
2021-03-22 00:23:50 +0100

    Create "materials" page PE-94

---
+++ frontend/src/pages/group/materials.js
@@ -0,0 +1,65 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { urlSearchParams } from "../../utils/functions.js"
+import URLS from "../../utils/urls.js"
+
+import Layout from "../../components/groupLayout.js"
+import TableForm from "../../components/tableForm.js"
+import Select from "../../components/select.js"
+
+// import classes from "./group.module.css"
+
+
+class FileInput extends React.Component {
+  render = () => <input type="file" name={this.props.name} />
+}
+
+
+export default () => {
+  const query = urlSearchParams()
+
+  // const platformId = query.get(`platformId`)
+  const groupId = query.get(`groupId`)
+  const href = `/group/it?platformId=${query.get(
+    "platformId"
+  )}&groupId=${groupId}`
+
+  return (
+    <Layout className="is-centered">
+      <Link className="return_link" to={href}>
+        Powrót do widoku grupy
+      </Link>
+
+      <h1>Grupa -- Oceny</h1>
+
+      <TableForm
+        fetchPostAddress={URLS.GROUPS$ID_FILE_POST.replace(`:groupId`, groupId)}
+        fetchGetAddress={URLS.GROUP$ID_NOTES_GET.replace(`:groupId`, groupId)}
+        fetchDeleteAddress={URLS.GROUP_NOTES$ID_DELETE}
+        enctype="multipart/form-data"
+        deleteIdParameterName=":noteId"
+        responseGetDataName="notes"
+        responsePostDataName="note"
+        buttonAdd="Wyślij plik"
+        buttonDelete="Usuń plik"
+        staticPostBodyData={{}}
+        objectsFields={[
+          `description`,
+          `file`,
+        ]}
+        titleFields={[`Opis`, `Plik`]}
+        inputFieldsComponents={{
+          file: {
+            component: FileInput,
+            props: {
+                name: `myFile`,
+            },
+
+          },
+        }}
+        colSpans={{ name: 2 }}
+      />
+    </Layout>
+  )
+}
