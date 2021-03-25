37d6ae8ee7263f1e1fb6aea112955ec9fbf1ed05
Adam Sztandar <10097355+asztandar@users.noreply.github.com>
2021-03-25 17:39:19 +0100

    Removal of unnecessary imports PE-112

--- frontend/src/pages/group/materials.js
+++ frontend/src/pages/group/materials.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import { Link } from "gatsby"
 
 import { urlSearchParams } from "../../utils/functions.js"
-import URLS, { HOST } from "../../utils/urls.js"
+import URLS from "../../utils/urls.js"
 import {getUser} from "../../utils/auth.js"
 
 import Layout from "../../components/groupLayout.js"
