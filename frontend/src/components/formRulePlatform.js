254b01b3806c4e36893d04874b24c0d70a6708ad
Adam Sztandar <10097355+asztandar@users.noreply.github.com>
2021-04-12 22:45:45 +0200

    Added GET permissions from server PE-115 PE-116 PE-117 PE-118 PE-119 PE-120

--- frontend/src/components/formRulePlatform.js
+++ frontend/src/components/formRulePlatform.js
@@ -1,29 +1,26 @@
 import React from "react"
-
+import URLS from "../utils/urls.js"
 import classes from "./formRule.module.css"
 import "./formRule.css"
-import { element } from "prop-types"
+import { getToken } from "../utils/auth.js"
+import { urlSearchParams } from "../utils/functions.js"
 export default class FormRulePlatform extends React.Component {
+  constructor(props) {
+    super(props)
+    const query = urlSearchParams()
+    this.platformId = query.get(`platformId`)
+  }
+
+
   state = {
     added: false,
     name: "",
     roleNameTab: {
-
-    }
+    },
+    permissions: [],
   }
 
-
-  templatePermissions = {
-    name: "",
-    color: "",
-    importance: "",
-    abilities: {
-      canEditDetails: "",
-      canDeletePlatform: "",
-      canManageGroups: "",
-      canManageUsers: "",
-    }
-  }
+  
 
   handleInputChange = event => {
     const target = event.target
@@ -37,23 +34,27 @@ export default class FormRulePlatform extends React.Component {
 
   handleButtonAddRole = (e) =>{
     e.preventDefault();
+    console.log(this.platformId)
     if(this.state.added === false){
-      const template = `
-      <td>
-        <input type="text" name="name" value="${this.state.name}" onChange={this.handleInputChange} />
-      </td>
-      ${["canEditDetails",
-      "canDeletePlatform",
-      "canManageGroups",
-      "canManageUsers"].map(ability => (
-        <td key={ability}>
-          <input type="checkbox" name={ability} onChange={e=>this.handleChange(e.target, this.state.name)}/>{" "}
-        </td>
-      ))}
-      `
+      const abilities = ["canEditDetails","canDeletePlatform","canManageGroups","canManageUsers"];
       const tableRef = document.getElementById('tableRoles').getElementsByTagName('tbody')[0];
-      var newRow = tableRef.insertRow(tableRef.rows.length);
-      newRow.innerHTML = template;
+      const newRow = tableRef.insertRow(tableRef.rows.length);
+      const nameTd = document.createElement(`td`);
+      const inputTd = document.createElement(`input`);
+      inputTd.type = `text`;
+      inputTd.name = `name`;
+      nameTd.appendChild(inputTd);
+      newRow.appendChild(nameTd);
+      abilities.forEach(ability => {
+        const td = document.createElement(`td`);
+        const input = document.createElement(`input`);
+
+        input.type = `checkbox`;
+        input.name = ability;
+        input.addEventListener(`change`, e=>this.handleChange(e.target, this.state.name));
+        td.appendChild(input);
+        newRow.appendChild(td);
+      })
       this.setState({added: true})
     }else{
       alert('Jednorazowo można dodać jedną rolę!')
@@ -68,77 +69,15 @@ export default class FormRulePlatform extends React.Component {
     }else{
       this.permissions.push({
         name: roleName,
-        color: null,
-        importance: 5,
         abilities: {
           [input.name]: input.checked,
         }
       })
     }
     console.log(this.permissions)
-
-    /*
-    const myTab = document.getElementById('tableRoles').getElementsByTagName('tbody')[0];
-    let name, color, importance, canEditDetails, canDeletePlatform, canManageGroups, canManageUsers;
-    const newRowTemp = `<input type="text" name="name" value="${this.state.name}" onChange={this.handleInputChange} />`;
-    console.log("myTab.rows.length: ", myTab.rows.length)
-    for (let i = 0; i < myTab.rows.length; i++) {
-      let objCells = myTab.rows.item(i).cells;
-      const tabObjCells = Array.from(objCells);
-      //console.log(tabObjCells)
-      name = tabObjCells[0].innerHTML === newRowTemp?this.state.name : tabObjCells[0].innerHTML;
-      color = null;
-      importance = 5;
-      canEditDetails = tabObjCells[1].getElementsByTagName('input')[0].checked ? true : false;
-      //if(tabObjCells[1].getElementsByTagName('input')[0].checked) console.log(tabObjCells[1].getElementsByTagName('input')[0])
-      
-      canDeletePlatform = tabObjCells[2].getElementsByTagName('input')[0].checked ? true : false;
-      canManageGroups = tabObjCells[3].getElementsByTagName('input')[0].checked ? true : false;
-      canManageUsers = tabObjCells[4].getElementsByTagName('input')[0].checked ? true : false;
-      console.log({canEditDetails, canDeletePlatform, canManageGroups, canManageUsers})
-      this.templatePermissions['name'] = name;
-      this.templatePermissions['color'] = color;
-      this.templatePermissions['importance'] = importance;
-      this.templatePermissions.abilities['canEditDetails'] = canEditDetails;
-      this.templatePermissions.abilities['canDeletePlatform'] = canDeletePlatform;
-      this.templatePermissions.abilities['canManageGroups'] = canManageGroups;
-      this.templatePermissions.abilities['canManageUsers'] = canManageUsers;
-      //console.log("templatePermissions",this.templatePermissions)
-      this.permissions.push(this.templatePermissions)
-      //console.log("permissions",this.permissions)
-    }
-    //console.log("permissions",this.permissions)
-    */
   }
 
   permissions = [];
-
-  /*
-  permissions=[
-    {
-      name: "owner",
-      color: null,
-      importance: 5,
-      abilities: {
-        canEditDetails: true,
-        canDeletePlatform: true,
-        canManageGroups: true,
-        canManageUsers: true,
-      }
-    },
-    {
-      name: "lecturer",
-      color: null,
-      importance: 5,
-      abilities: {
-        canEditDetails: false,
-        canDeletePlatform: false,
-        canManageGroups: true,
-        canManageUsers: true,
-      }
-    },
-  ]
-*/
 
   availableRoles = {
     canEditDetails: "Edycja szczegółów",
@@ -148,6 +87,29 @@ export default class FormRulePlatform extends React.Component {
     key: function(n) {
       return this[Object.keys(this)[n]];
     }
+  }
+
+  async componentDidMount(){
+    console.log("adres GETa: ", URLS.PLATFORM$ID_PERMISSIONS_GET.replace(`:platformId`,this.platformId))
+    const {permissions} = await fetch(URLS.PLATFORM$ID_PERMISSIONS_GET.replace(`:platformId`,this.platformId),{
+      method: 'GET',
+      headers: { Authentication: `Bearer ${getToken()}` }
+    }).then(res => res.json())
+    const trs =  permissions.map(({name, abilities}) => (
+        <tr key={name}>
+          <td>{name}</td>
+            {Object.entries(abilities).map(([ability, isChecked]) => (
+                <td key={ability}>
+                    <input defaultChecked={isChecked} type="checkbox" name={ability} onChange={e=>this.handleChange(e.target, name)}/>
+                </td>
+            ))}
+        </tr>
+    ))
+    this.setState({permissions: trs})
+    
+
+    
+
   }
   
   render = () => (
@@ -165,39 +127,7 @@ export default class FormRulePlatform extends React.Component {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Właściciel</td>
-            {["canEditDetails",
-              "canDeletePlatform",
-              "canManageGroups",
-              "canManageUsers"].map(ability => (
-                <td key={ability}>
-                  <input type="checkbox" name={ability} onChange={e=>this.handleChange(e.target, "Właściciel")}/>{" "}
-                </td>
-              ))}
-          </tr>
-          <tr>
-            <td>Nauczyciel</td>
-            {["canEditDetails",
-              "canDeletePlatform",
-              "canManageGroups",
-              "canManageUsers"].map(ability => (
-                <td key={ability}>
-                  <input type="checkbox" name={ability} onChange={e=>this.handleChange(e.target, "Nauczyciel")}/>{" "}
-                </td>
-              ))}
-          </tr>
-          <tr>
-            <td>Student</td>
-            {["canEditDetails",
-              "canDeletePlatform",
-              "canManageGroups",
-              "canManageUsers"].map(ability => (
-                <td key={ability}>
-                  <input type="checkbox" name={ability} onChange={e=>this.handleChange(e.target, "Student")}/>{" "}
-                </td>
-              ))}
-          </tr>
+          {this.state.permissions}
           </tbody>
           <tfoot>
           <tr>
