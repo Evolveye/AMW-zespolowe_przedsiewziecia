d48024ee10f639e14e89f28810c326d21e9cfa4d
Adam Sztandar <10097355+asztandar@users.noreply.github.com>
2021-04-13 22:56:59 +0200

    Fixed problem with added new role PE-120

--- frontend/src/components/formRulePlatform.js
+++ frontend/src/components/formRulePlatform.js
@@ -13,6 +13,8 @@ export default class FormRulePlatform extends React.Component {
 
   state = {
     added: false,
+    checked: false,
+    checkedNew: false,
     name: "",
     roleNameTab: {
     },
@@ -23,12 +25,17 @@ export default class FormRulePlatform extends React.Component {
   
 
   handleInputChange = event => {
+    console.log("pisze")
     const target = event.target
     const value = target.value
     const name = target.name
     this.setState({
       [name]: value,
     })
+  }
+
+  handleCheckedNew = () =>{
+    this.setState({checkedNew: true})
   }
 
 
@@ -44,7 +51,7 @@ export default class FormRulePlatform extends React.Component {
       inputTd.type = `text`;
       inputTd.name = `name`;
       inputTd.value = this.state.name;
-      inputTd.addEventListener('change', this.handleInputChange);
+      inputTd.addEventListener('input',this.handleInputChange);
       nameTd.appendChild(inputTd);
       newRow.appendChild(nameTd);
       abilities.forEach(ability => {
@@ -53,7 +60,7 @@ export default class FormRulePlatform extends React.Component {
 
         input.type = `checkbox`;
         input.name = ability;
-        input.addEventListener(`change`, e=>this.handleChange(e.target, this.state.name));
+        input.addEventListener(`change`, e=>this.handleChange(e.target, this.state.name), 'change', this.handleCheckedNew );
         td.appendChild(input);
         newRow.appendChild(td);
       })
@@ -65,6 +72,7 @@ export default class FormRulePlatform extends React.Component {
 
   handleChange = (input, roleName) =>{
     this.enableButton();
+    this.setState({checked: true});
     const permission = this.permissions.find(({name}) => name === roleName)
     if(permission){
       permission.abilities[input.name] = input.checked
@@ -80,30 +88,57 @@ export default class FormRulePlatform extends React.Component {
   }
 
   handleSubmit = (e) => {
-    e.preventDefault();
+    
+    this.disableButton();
     //check that Dodaj rolę button was clicked
     if(this.state.added === false){
       //not clicked
-      this.postDataPermissions();
+      this.putDataPermissions();
     }else{
       //clicked
       if(this.state.name.length <= 0){
         //wrong - without name
         alert("UWAGA! Nie można wysłać roli bez nazwy!")
       }else{
-        this.postDataPermissions();
+        console.log("checked: ", this.state.checked)
+        if(this.state.checked) {this.putDataPermissions()};
+        if(this.state.added && this.state.checkedNew) {this.postDataPermissions(1)};
+        if(this.state.added && !this.state.checkedNew) {this.postDataPermissions(0)};
+        alert("Aby zobaczyć zmiany odśwież stronę!")
       }
     }
   }
 
-  postDataPermissions = () =>{
-    fetch(URLS.PLATFORM$ID_PERMISSIONS_POST,{
+  putDataPermissions = () =>{
+    console.log("PID",this.platformId)
+    console.log("URL", URLS.PLATFORM$ID_PERMISSIONS_POST.replace(`:platformId`,this.platformId))
+    console.log("SEND",{update: this.permissions})
+    fetch(URLS.PLATFORM$ID_PERMISSIONS_POST.replace(`:platformId`,this.platformId),{
+      method: `PUT`,
+      headers: {
+        Authentication: `Bearer ${getToken()}`,
+        "Content-Type": `application/json`,
+      },
+      body: JSON.stringify({array:this.permissions}),
+    }).then(res => res.json())
+    console.log("to co wysyłam: ",this.permissions)
+    console.log("poszło put!")
+  }
+
+  postDataPermissions = (checked) =>{
+    let perm = "";
+    if(checked == 1) perm = this.permissions
+    if(checked == 0) perm = {name: this.state.name}
+    fetch(URLS.PLATFORM$ID_PERMISSIONS_POST.replace(`:platformId`,this.platformId),{
       method: `POST`,
-      headers: { Authentication: `Bearer ${getToken()}` },
-      body: this.permissions,
-    }).then(res => res.json()).then(response => console.log(response))
-    console.log(this.permissions)
-    console.log("poszło!")
+      headers: {
+        Authentication: `Bearer ${getToken()}`,
+        "Content-Type": `application/json`,
+      },
+      body: JSON.stringify({perm:perm}),
+    }).then(res => res.json())
+    console.log("to co wysyłam: ",JSON.stringify({array:this.state.name}))
+    console.log("poszło post!")
   }
 
   availableRoles = {
@@ -120,6 +155,10 @@ export default class FormRulePlatform extends React.Component {
     const button =  document.querySelector('#button-Zapisz')
     button.disabled = false;
   }
+  disableButton = () =>{
+      const button =  document.querySelector('#button-Zapisz')
+      button.disabled = true;
+  }
   async componentDidMount(){
     console.log("adres GETa: ", URLS.PLATFORM$ID_PERMISSIONS_GET.replace(`:platformId`,this.platformId))
     const button =  document.querySelector('#button-Zapisz')
@@ -128,6 +167,9 @@ export default class FormRulePlatform extends React.Component {
       method: 'GET',
       headers: { Authentication: `Bearer ${getToken()}` }
     }).then(res => res.json())
+
+    console.log("permissions: ",permissions)
+
     const trs =  permissions.map(({name, abilities}) => (
         <tr key={name}>
           <td>{name}</td>
