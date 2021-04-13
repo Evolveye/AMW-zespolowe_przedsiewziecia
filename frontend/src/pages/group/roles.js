672d3126668f3b736869b22da2df9141ea84a674
Adam Sztandar <10097355+asztandar@users.noreply.github.com>
2021-04-13 22:55:05 +0200

    Added Role page in group PE-123

--- frontend/src/pages/group/roles.js
+++ frontend/src/pages/group/roles.js
@@ -1,25 +1,31 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import FormRuleGroup from "../../components/formRuleGroup.js"
 import { urlSearchParams } from "../../utils/functions.js"
 
 import Layout from "../../components/groupLayout.js"
 
 // import classes from "./group.module.css"
 
-export default () => {
-  const query = urlSearchParams()
-  const href = `/group/it?platformId=${query.get(
-    "platformId"
-  )}&groupId=${query.get("groupId")}`
+export default class GroupRules extends React.Component {
+  constructor(props) {
+    super(props)
+    const query = urlSearchParams()
+    this.platformId = query.get(`platformId`)
+    this.groupId = query.get(`groupId`)
+    this.href = `/group/it?platformId=${this.platformId}&groupId=${this.groupId}`
+  }
 
-  return (
+  render = () => (
     <Layout className="is-centered">
-      <Link className="return_link" to={href}>
+      <Link className="return_link" to={this.href}>
         Powrót do widoku grupy
       </Link>
 
       <h1>Grupa -- role</h1>
+      <div>
+        <FormRuleGroup />
+      </div>
     </Layout>
   )
 }
